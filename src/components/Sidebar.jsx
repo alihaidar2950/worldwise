5b7c8b7496557643442a0ded4e37677bde690993
alihaidar2950 <alihaidar2950@gmail.com>
2023-11-26 14:10:45 -0500

    Nested Routes and Index Route

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import Logo from './Logo.jsx';
 import AppNav from './AppNav.jsx';
 import styles from './Sidebar.module.css';
+import { Outlet } from 'react-router-dom';
 
 function Sidebar() {
   return (
@@ -8,7 +9,8 @@ function Sidebar() {
       <Logo />
       <AppNav />
 
-      <p>List of cities</p>
+      <Outlet />
+
       <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
