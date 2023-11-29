8683230419e89f4f4351f12c2fa09b595437dccd
alihaidar2950 <alihaidar2950@gmail.com>
2023-11-28 20:42:41 -0500

    Implementing the Countries List

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,11 +1,16 @@
 /* eslint-disable react/prop-types */
 import Spinner from './Spinner';
+import Message from './Message';
 import styles from './CityList.module.css';
 import CityItem from './CityItem';
 
 function CityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
 
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
   return (
     <ul className={styles.cityList}>
       {cities.map((city) => (
